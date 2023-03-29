599d8ad2724fd3947d48a7a20108bb7732bf98cf
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-03-29 15:35:36 -0300

    Terminando aula 1

---
+++ rafael-stickers/src/App.java
@@ -0,0 +1,32 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.security.Key;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Conexão HTTP
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI addresUri = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(addresUri).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> filmList = parser.parse(body);
+        
+        // exibe
+        for (Map<String,String> filmMap : filmList) {            
+            System.out.println(filmMap.get("title"));
+            System.out.println(filmMap.get("image"));
+            System.out.println(filmMap.get("imDbRating"));
+        }
+    }
+}
