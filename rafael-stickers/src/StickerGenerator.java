8b83c51a3b11a8707365c05c358450a8910777fb
Rafael Eduardo Ronchi Filho <rafael.e01@aluno.ifsc.edu.br>
2023-03-29 23:15:36 -0300

    Começando aula 2

---
+++ rafael-stickers/src/StickerGenerator.java
@@ -0,0 +1,21 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class StickerGenerator {
+    
+    public void generate() throws IOException{
+        //Ler img
+        BufferedImage originalImg = ImageIO.read(new File("entrada/TopMovies_1.jpg"));
+
+        // Cria nova img
+
+        // Copiar img original para nova img 
+
+        // Escrever frase
+        
+        // Escrever a nova imagem
+    }
+}
