22cc00974fcdaee6e5c9e79a0f87a779ff3c2e64
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-04-02 00:15:07 -0300

    LIgando a API com MONGODB para gerar um json editavel

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,21 +1,31 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class LinguagemController {
-
-    private List<Linguagem> linguagens=
-    List.of(
-        new Linguagem("Java", "https://github.com/abrahamcalf/programming-languages-logos/blob/master/src/java/java_256x256.png?raw=true",  1), 
-        new Linguagem("C#", "https://github.com/abrahamcalf/programming-languages-logos/blob/master/src/csharp/csharp_256x256.png?raw=true", 2)
-    );
+    
+    @Autowired
+    private LinguagemRepository repositorio;
 
     @GetMapping("/linguagens")
-    public List<Linguagem> ObterLinguagens(){
+    public List<Linguagem> obterLinguagens() {
+        List<Linguagem> linguagens = repositorio.findAll();
         return linguagens;
     }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
+    } 
+
 }
